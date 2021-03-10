d4b2a72f3a7c2ef4e858e3ef9e574eb01a3b80cb
Guliaiev Andrey <andrguliaiev@gmail.com>
2021-03-10 19:51:13 +0300

    bonus

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        long score = 100;
+        long payment = 1200;
+        boolean Bonus = payment > 1000;
+
+        if (Bonus) {
+            long bonus = payment / 100;
+            long total = score + payment + bonus;
+            System.out.println("Бонус: " + bonus + " Итоговая сумма на счету клиента: " + total);
+
+        } else {
+            long total = score + payment;
+            System.out.println("Бонус: 0" + " Итоговая сумма на счету клиента: " + total);
+        }
+    }
+}
